44dcf20d7d7b46bc18531bdfa1f36829faec8cd9
borashek <borashek@inbox.ru>
2023-06-19 12:14:09 +0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  ActivityIndicator,
+  Button, Dimensions,
+  FlatList,
+  Image, ListRenderItem,
+  ScrollView,
+  StyleSheet,
+  Text, TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import {useState} from "react"
+
+
+const {width, height} = Dimensions.get('screen')
+// best practice
+const WIDTH = width // all width
+const HEIGHT = height // all height
+
+const titles = ['iphone', 'samsung', 'siemens', 'nokia']
+const prices = [234, 674, 890, 564]
+
+type ItemType = {
+  id: number,
+  title: string,
+  price: number
+}
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1}}>
+      <View style={{flex: 3, backgroundColor: 'red'}} />
+      <View style={{flex: 2, backgroundColor: 'yellow'}} />
+      <View style={{flex: 1, backgroundColor: 'white'}} />
     </View>
   );
 }
@@ -15,6 +42,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
